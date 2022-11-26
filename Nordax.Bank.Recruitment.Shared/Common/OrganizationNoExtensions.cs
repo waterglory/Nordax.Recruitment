186ebd7869072d454fe12ed7121793e0b80c5118
Andy Tirtajaya <andywaterglory@outlook.com>
2022-11-26 06:31:41 +0100

    Ensure that only 10 digits organization no is used for data consistency.

--- Nordax.Bank.Recruitment.Shared/Common/OrganizationNoExtensions.cs
+++ Nordax.Bank.Recruitment.Shared/Common/OrganizationNoExtensions.cs
@@ -10,8 +10,13 @@ namespace Nordax.Bank.Recruitment.Shared.Common
 {
 	public static class OrganizationNoExtensions
 	{
-		public static string CleanUpOrganizationNo(this string organizationNo) =>
-			organizationNo.Replace("-", string.Empty);
+		public static string CleanUpOrganizationNo(this string organizationNo)
+		{
+			organizationNo = organizationNo.Replace("-", string.Empty);
+			if (organizationNo.Length > 10)
+				organizationNo.Substring(organizationNo.Length - 10);
+			return organizationNo;
+		}
 
 		public static void CleanUpOrganizationNo(this LoanApplicationModel loanApplicationModel) =>
 			loanApplicationModel.Applicant.OrganizationNo = loanApplicationModel.Applicant.OrganizationNo.CleanUpOrganizationNo();
